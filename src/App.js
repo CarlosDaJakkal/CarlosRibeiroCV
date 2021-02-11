0cc3f0054035381638e1e4e0c8adb30992e0a573
BLACKTOP\CarlosDaJakkal <carlosdajakkal@gmail.com>
2021-02-11 17:00:36 +0000

    * cleaned up `App.js`

--- src/App.js
+++ src/App.js
@@ -1,30 +1,28 @@
-import React, { Component } from 'react';
-import NavBar from './components/navbar';
-import PersonalDetail from './components/personalDetail'
-import MainSections from './components/mainSections'
-import SideSections from './components/sideSections'
-// import './App.css';
+import React, { Component } from "react";
+import NavBar from "./components/navbar";
+import PersonalDetail from "./components/personalDetail";
+import MainSections from "./components/mainSections";
+import SideSections from "./components/sideSections";
 
 class App extends Component {
-   
-  render() {
-     return (
-      <React.Fragment>
-        <NavBar />
-        <main className="container">
-            <PersonalDetail />    
-            <div className="row">
-                <div className="col-md-9">
-                    <MainSections /> 
-                </div>
-                <div className="col-md-3">
-                    <SideSections />
-                </div>
-            </div>
-        </main>
-      </React.Fragment>
-    );
-  }
+    render() {
+        return (
+            <React.Fragment>
+                <NavBar />
+                <main className="container">
+                    <PersonalDetail />
+                    <div className="row">
+                        <div className="col-md-9">
+                            <MainSections />
+                        </div>
+                        <div className="col-md-3">
+                            <SideSections />
+                        </div>
+                    </div>
+                </main>
+            </React.Fragment>
+        );
+    }
 }
- 
+
 export default App;
